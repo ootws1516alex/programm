970a6ccb66be82985add47f5cf8ad1dd4f3fcddc
Saueee <t.sauer1@gmx.de>
2015-11-27 17:43:53 +0100

    Klassen erstellt

---
+++ src/tobias/test/HumanVSHuman.java
@@ -0,0 +1,5 @@
+package tobias.test;
+
+public class HumanVSHuman {
+
+}
---
+++ src/tobias/test/Menue.java
@@ -0,0 +1,10 @@
+package tobias.test;
+
+public class Menue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
